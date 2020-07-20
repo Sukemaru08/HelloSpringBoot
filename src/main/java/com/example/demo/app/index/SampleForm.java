9fd35fbe0b5ea6700a932c766fab009bbed28c7a
Sukemaru <narumi.anm18@gmail.com>
2020-07-20 17:50:24 +0900

    フォームの送信追加

--- src/main/java/com/example/demo/app/index/SampleForm.java
+++ src/main/java/com/example/demo/app/index/SampleForm.java
@@ -4,10 +4,22 @@ import lombok.Data;
 
 @Data
 public class SampleForm {
-    private String input;
-    private boolean checkbox1;
-    private boolean checkbox2;
-    private int radio;
-    private int select;
-    public SampleForm() {}
+
+    private String name;
+    private String email;
+    private String contents;
+
+    //それぞれを引数とするコンストラクタ
+    public SampleForm(String Name, String Email, String Contents){
+        this.name = Name;
+        this.email = Email;
+        this.contents = Contents;
+    }
+
+    //引数なしのコンストラクタ
+    public SampleForm(){
+        this("","","");
+    }
+
+    //getter, setterは@Dataで自動生成
 }
