e31a1a9ea1d1b15008ebc91b2468ae9e38630631
Sukemaru <narumi.anm18@gmail.com>
2020-07-22 10:24:13 +0900

    バリデーション（エラーメッセージなし）追加 ＊入力エラーの場合はindex.htmlに戻る

--- src/main/java/com/example/demo/app/index/IndexController.java
+++ src/main/java/com/example/demo/app/index/IndexController.java
@@ -1,7 +1,11 @@
 package com.example.demo.app.index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +21,10 @@ public class IndexController {
 
     //入力確認画面へ遷移
     @PostMapping("/confirm")
-    public String confirm(SampleForm sampleForm){
+    public String confirm(@Validated SampleForm sampleForm, BindingResult result){
+        if(result.hasErrors()){
+            return "inquiry/index.html";
+        }
         return "inquiry/confirm.html";
     }
 }
--- src/main/java/com/example/demo/app/index/SampleForm.java
+++ src/main/java/com/example/demo/app/index/SampleForm.java
@@ -2,11 +2,24 @@ package com.example.demo.app.index;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class SampleForm {
 
+    @NotBlank(message = "お名前を入力してください")
+    @Size(max = 65, message = "お名前は65文字以内で入力してください")
     private String name;
+
+    @NotBlank(message = "メールアドレスを入力してください")
+    @Size(max = 128, message = "メールアドレスは128文字以内で入力してください")
+    @Email
     private String email;
+
+    @NotBlank(message = "お問い合わせ内容を入力してください")
+    @Size(max = 500, message = "お問い合わせ内容は500文字以内で入力してください")
     private String contents;
 
     //それぞれを引数とするコンストラクタ
